c85fc94f8cbbb92973e9412116b482b0a3f01255
Kirill <kirillsavorin@gmail.com>
2025-11-26 23:03:20 +0100

    ReportController for POST /api/reports

---
+++ backend/src/main/java/org/fungover/zipp/controller/ReportController.java
@@ -0,0 +1,40 @@
+package org.fungover.zipp.controller;
+
+import org.fungover.zipp.dto.Report;
+import org.fungover.zipp.service.ReportService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reports")
+public class ReportController {
+
+  private static final Logger log = LoggerFactory.getLogger(ReportController.class);
+  private final ReportService reportService;
+
+  public ReportController(ReportService reportService) {
+    this.reportService = reportService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Report> createReport(@RequestBody Report reportRequest) {
+    log.info("Report received: {}", reportRequest);
+
+    var newReport = reportService.createReport(reportRequest);
+
+    /* For now the userId is provided by the client
+     later this can be replaced with SecurityContextHolder.getContext().getAuthentication()
+
+     And Spring kafka later
+     var cf = template.send("report", newReport);
+      cf.join();
+     */
+
+    return ResponseEntity.status(201).body(newReport);
+  }
+}
