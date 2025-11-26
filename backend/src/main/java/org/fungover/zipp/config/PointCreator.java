77035e1c3765be608ae55959a8b13173593cf2eb
Kirill <kirillsavorin@gmail.com>
2025-11-26 21:42:07 +0100

    Reports & Images entity New dependency Flyway migration New Repository

---
+++ backend/src/main/java/org/fungover/zipp/config/PointCreator.java
@@ -0,0 +1,13 @@
+package org.fungover.zipp.config;
+
+import org.locationtech.jts.geom.GeometryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PointCreator {
+  @Bean
+  public GeometryFactory geometryFactory() {
+    return new GeometryFactory();
+  }
+}
---
+++ backend/src/main/java/org/fungover/zipp/repository/ReportRepository.java
@@ -0,0 +1,7 @@
+package org.fungover.zipp.repository;
+
+import org.fungover.zipp.entity.ReportEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
+}
