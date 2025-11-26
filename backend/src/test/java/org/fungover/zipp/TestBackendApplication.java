571230993e2081f100f1c6515ef98129d4984204
Brannar3 <alfred-brannare@hotmail.com>
2025-11-26 10:54:58 +0100

    Setup/initial Project Structure (#12)
    * Add Spring Boot backend module with MySQL and Testcontainers
    
    * Update PR suggestions + some more
    - Fixed "Environment" typo in .gitignore
    - Added some basic config in application.properties
    - Added a application-dev.properties
    - Changed package name to org.fungover.zipp
    - Added a docker-compose.yml
    
    * Remove docker-compose.yml and update application.properties & application-dev.properties
    
    * Revert "Remove docker-compose.yml and update application.properties & application-dev.properties"
    
    This reverts commit 7647de6b0da424f0393e80d4543230ad436ef9c1.
    
    * Resolve CodeRabbit nagging

---
+++ backend/src/test/java/org/fungover/zipp/TestBackendApplication.java
@@ -0,0 +1,11 @@
+package org.fungover.zipp;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(BackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
