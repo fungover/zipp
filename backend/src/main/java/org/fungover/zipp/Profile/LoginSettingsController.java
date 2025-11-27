371199c8bd2bf6c30f4eb2a412875e7e0d594f58
Christian <christianfryksten1@gmail.com>
2025-11-27 11:19:32 +0100

    Inital commit
    Co-authored-by: Nishune <rikardengstrom89@gmail.com>
    Co-authored-by: marcusmoller97 <marcusssmoller@gmail.com>
    Co-authored-by: SpinalGlitter <johannes.persson47@gmail.com>

---
+++ backend/src/main/java/org/fungover/zipp/Profile/LoginSettingsController.java
@@ -0,0 +1,13 @@
+package org.fungover.zipp.Profile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginSettingsController {
+
+    @GetMapping("/loginsettings")
+    public String loginSettings() {
+        return "loginsettings";  // this maps to loginsettings.html in templates
+    }
+}
