ff4455a2f3b3018c6b9cf0af00ad734c1674feb6
Christian <christianfryksten1@gmail.com>
2025-11-27 13:37:45 +0100

    Dummy user created with flyway
    Co-authored-by: Nishune <rikardengstrom89@gmail.com>
    Co-authored-by: marcusmoller97 <marcusssmoller@gmail.com>
    Co-authored-by: SpinalGlitter <johannes.persson47@gmail.com>

---
+++ backend/src/main/resources/db/migration/V2__create_dummy_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO user_profile (bio, city, address, display_name)
+VALUES (
+           'Just a sample user profile for initial development and testing.',
+           'Piteå',
+           'Hejvägen123',
+        'John Doe'
+       );
