58a9da5a6fb747c4cda923295cc6432dcdf92b4d
jennymakki <jjennyeklundd@hotmail.com>
2025-11-29 15:40:25 +0100

    feat: added basic rate limiting interceptor

---
+++ backend/src/main/java/org/fungover/zipp/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.fungover.zipp.config;
+
+import org.fungover.zipp.interceptor.RateLimitingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private RateLimitingInterceptor rateLimitingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitingInterceptor)
+            .addPathPatterns("/api/...");
+    }
+}
