d121ccb44cfcd59f29ab8e99d99009ed30b5d46e
Jörgen Lindström <jorgen.lindstrom@lerniastudent.se>
2025-12-03 10:12:00 +0100

    Hunted errors in pom.xml, BackendApplication, checkstyle.xml manualy tested with mvn spotless:check mvn checkstyle:check mvn clean verify mvn pmd:check

--- backend/src/main/java/org/fungover/zipp/BackendApplication.java
+++ backend/src/main/java/org/fungover/zipp/BackendApplication.java
@@ -4,20 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class BackendApplication {
-
-    private BackendApplication() {
-        throw new UnsupportedOperationException
-                ("This is a utility class and cannot be instantiated");
-    }
+public class BackendApplication {
 
     /**
      * Entry point for the backend application.
      *
-     * @param args the command line arguments
+     * @param args
+     *            the command line arguments
      */
-
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 }
--- backend/src/main/java/org/fungover/zipp/package-info.java
+++ backend/src/main/java/org/fungover/zipp/package-info.java
@@ -1,7 +1,5 @@
 /**
- * This file is for Package level documentation
- * This package contains the core backend logic for the Zipp application.
- **/
-
+ * This file is for Package level documentation This package contains the core
+ * backend logic for the Zipp application.
+ */
 package org.fungover.zipp;
-
