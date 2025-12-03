66957d1c458cc423b79e286e4e0862a026aacab4
Jörgen Lindström <jorgen.lindstrom@lerniastudent.se>
2025-12-03 14:18:29 +0100

    deleted public in AuthenticationIntegrationTest

--- backend/src/test/java/org/fungover/zipp/authtest/AuthenticationIntegrationTest.java
+++ backend/src/test/java/org/fungover/zipp/authtest/AuthenticationIntegrationTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Import(TestcontainersConfiguration.class)
-public class AuthenticationIntegrationTest {
+class AuthenticationIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
