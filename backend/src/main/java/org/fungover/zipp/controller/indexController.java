dec909748de9eca94bf50c006939e2d9505fd9b3
RobTrb <Robert.trb@gmail.com>
2025-12-03 10:58:03 +0100

    Login works now. And the root page / now shows the users name just to confirm that it works and all comes through. Page is now ready for further implementation.

--- backend/src/main/java/org/fungover/zipp/controller/indexController.java
+++ backend/src/main/java/org/fungover/zipp/controller/indexController.java
@@ -3,13 +3,29 @@ package org.fungover.zipp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Controller
 public class indexController {
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, Authentication authentication) {
         model.addAttribute("title", "Zipp");
+
+        if (authentication != null && authentication.isAuthenticated()){
+            if (authentication.getPrincipal() instanceof OAuth2User oAuth2User) {
+
+                OAuth2User oauth2user = (OAuth2User) authentication.getPrincipal();
+
+                String userName = oauth2user.getAttribute("name");
+
+                model.addAttribute("userName", userName);
+                model.addAttribute("isLoggedIn", true);
+            }
+        } else {
+            model.addAttribute("isLoggedIn", false);
+        }
         return "index";
     }
 
