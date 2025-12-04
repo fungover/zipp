f4c4973aa1baa0c1d4c06b9c95a267e72973c536
RobTrb <Robert.trb@gmail.com>
2025-12-04 13:20:12 +0100

    Merged main into branch then made new commit

--- backend/src/main/java/org/fungover/zipp/controller/indexController.java
+++ backend/src/main/java/org/fungover/zipp/controller/indexController.java
@@ -1,13 +1,13 @@
 package org.fungover.zipp.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Controller
-public class indexController {
+public class IndexController {
 
     @GetMapping("/")
     public String index(Model model, Authentication authentication) {
