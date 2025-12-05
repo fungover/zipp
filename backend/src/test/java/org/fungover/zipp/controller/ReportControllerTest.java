0801b9f8af66a6e62a435517c17ce46c89f8220a
Louise <emmalouisekarlsson@hotmail.com>
2025-12-05 10:40:34 +0100

    Added test for badRequest for post and returns report for get

--- backend/src/test/java/org/fungover/zipp/controller/ReportControllerTest.java
+++ backend/src/test/java/org/fungover/zipp/controller/ReportControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.fungover.zipp.dto.Report;
 import org.fungover.zipp.dto.ReportStatus;
 import org.fungover.zipp.dto.ReportType;
+import org.fungover.zipp.service.ReportService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,13 +14,13 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 import java.time.Instant;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,6 +28,9 @@ class ReportControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ReportService reportService;
 
     @Test
     void createReport() throws Exception {
@@ -44,14 +48,53 @@ class ReportControllerTest {
                         user.put("role", "USER");
                     }))
                 .with(csrf())
-            //POST isn't blocked by spring security with csfr
+            //POST isn't blocked by spring security with csrf
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(firstReport)))
-            .andExpect(status().isCreated());
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.description").value("Candy paper"));
+    }
+
+    @Test
+    void throwsErrorWhenRequestIsNotValid()throws Exception {
+        Report firstReport = new Report(1L,null, ReportType.DEBRIS,
+            50.0, 50.0, Instant.parse("2025-12-03T15:30:00Z"), ReportStatus.ACTIVE, null);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        mockMvc.perform(post("/api/reports")
+                .with(SecurityMockMvcRequestPostProcessors.oauth2Login()
+                    .attributes(user -> {
+                        user.put("name", "Test User");
+                        user.put("email", "test@gmail.com");
+                        user.put("role", "USER");
+                    }))
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(firstReport)))
+            .andExpect(status().isBadRequest());
 
     }
 
     @Test
-    void getAllReports() {
+    void returnsSubmittedReport() throws Exception {
+        Report firstReport = new Report(1L,"Candy paper", ReportType.DEBRIS,
+            50.0, 50.0, Instant.parse("2025-12-03T15:30:00Z"), ReportStatus.ACTIVE, null);
+
+      reportService.createReport(firstReport);
+
+        mockMvc.perform(get("/api/reports")
+                .with(SecurityMockMvcRequestPostProcessors.oauth2Login()
+                    .attributes(user -> {
+                        user.put("name", "Test User");
+                        user.put("email", "test@gmail.com");
+                        user.put("role", "USER");
+                    }))
+                .with(csrf()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].status").value("ACTIVE"));
+
+
     }
 }
