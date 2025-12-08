1b8ecda5440540aa54a81ffa87df9fb2cc68d7e7
RobTrb <Robert.trb@gmail.com>
2025-12-08 21:45:30 +0100

    removed wildcard import and changed to explicit static imports in the test file

--- backend/src/test/java/org/fungover/zipp/authtest/AuthenticationIntegrationTest.java
+++ backend/src/test/java/org/fungover/zipp/authtest/AuthenticationIntegrationTest.java
@@ -17,7 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @SpringBootTest
 @AutoConfigureMockMvc
