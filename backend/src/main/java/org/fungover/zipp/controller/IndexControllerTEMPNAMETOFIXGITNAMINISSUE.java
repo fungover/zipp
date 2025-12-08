1657f1bd4870d294362c1b665a0ffa0922f01857
RobTrb <Robert.trb@gmail.com>
2025-12-08 15:01:53 +0100

    more changes to match main branch again

--- backend/src/main/java/org/fungover/zipp/controller/IndexControllerTEMPNAMETOFIXGITNAMINISSUE.java
+++ backend/src/main/java/org/fungover/zipp/controller/IndexControllerTEMPNAMETOFIXGITNAMINISSUE.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class IndexControllerTEMPNAMETOFIXGITNAMINISSUE {
 
     @GetMapping("/")
     public String index(Model model, Authentication authentication) {
