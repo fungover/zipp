90b4589b4adb70a630560ed3b82e18291bde02e6
Tobias Larsson <tobbe-larsson@hotmail.com>
2025-12-10 15:45:19 +0100

    Improve formatting in `SseService`, `KafkaConfig`, and `EventListenerServiceTest` for better readability and consistency

--- backend/src/test/java/org/fungover/zipp/service/EventListenerServiceTest.java
+++ backend/src/test/java/org/fungover/zipp/service/EventListenerServiceTest.java
@@ -1,7 +1,9 @@
 package org.fungover.zipp.service;
 
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 
 class EventListenerServiceTest {
 
