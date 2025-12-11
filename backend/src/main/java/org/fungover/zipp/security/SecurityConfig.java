f6e797ba312f0e9f0e36c93921d3e52ead2ccc77
Louise <emmalouisekarlsson@hotmail.com>
2025-12-11 21:19:01 +0100

    Adjustments index, layout and SecurityConfig

--- backend/src/main/java/org/fungover/zipp/security/SecurityConfig.java
+++ backend/src/main/java/org/fungover/zipp/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,9 +19,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/login", "/favicon.ico", "/favicon/**", "/css/**", "/images/**", "/js/**")
-                .permitAll().anyRequest().authenticated())
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/reports").authenticated()
+                        .requestMatchers("/", "/login", "/css/**", "/images/**", "/js/**").permitAll().anyRequest()
+                        .authenticated())
                 .oauth2Login(oauth2 -> oauth2.loginPage("/login").defaultSuccessUrl("/", true)
                         .userInfoEndpoint(userInfo -> userInfo.userService(co2us)))
                 .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/").invalidateHttpSession(true)
