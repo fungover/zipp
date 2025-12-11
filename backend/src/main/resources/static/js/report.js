71327e8c4284f027ee6f9131c4de4f7c381181fe
Louise <emmalouisekarlsson@hotmail.com>
2025-12-11 08:39:07 +0100

    function to retrieve location

--- backend/src/main/resources/static/js/report.js
+++ backend/src/main/resources/static/js/report.js
@@ -1,10 +1,38 @@
+function getLocation(){
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 25000,
+  }
+  navigator.geolocation.getCurrentPosition(success, error, options)
+
+  function success(position){
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    console.log(latitude, longitude)
+  }
+
+  function error(position){
+    alert("Error: "+position);
+  }
+
+}
+
+function getUserLocation(){
+  fetch('http://ip-api.com/json/?fields=61439')
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
 const button = document.querySelectorAll(".report__button");
 
 button.forEach(button => {
   button.addEventListener("click", (e) => {
     e.preventDefault();
     const report = e.currentTarget.value;
-    console.log(report);
+
+    getLocation();
+    getUserLocation()
 
     fetch("/api/reports", {
       method: "POST",
