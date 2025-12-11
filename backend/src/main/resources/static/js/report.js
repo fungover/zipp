427efc223e571ff045bdc298e0a7dbd29dfff16a
Louise <emmalouisekarlsson@hotmail.com>
2025-12-11 13:48:48 +0100

    Sends a post with description, url and position

--- backend/src/main/resources/static/js/report.js
+++ backend/src/main/resources/static/js/report.js
@@ -1,60 +1,47 @@
-function getLocation(){
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 25000,
-  }
-  navigator.geolocation.getCurrentPosition(success, error, options)
+import getLocation from "./getLocation.js";
 
-  function success(position){
-    const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
-    console.log(latitude, longitude)
-  }
-
-  function error(position){
-    alert("Error: "+position);
-  }
-
-}
-
-function getUserLocation(){
-  fetch('http://ip-api.com/json/?fields=61439')
-    .then(response => response.json())
-    .then(data => console.log(data))
-}
-
+const form = document.querySelector(".report");
 const button = document.querySelectorAll(".report__button");
 
 button.forEach(button => {
-  button.addEventListener("click", (e) => {
+  button.addEventListener("click", async (e) => {
     e.preventDefault();
     const report = e.currentTarget.value;
 
-    getLocation();
-    getUserLocation()
+    if(!form.checkValidity()) {
+      form.reportValidity();
+      return;
+    }
 
-    fetch("/api/reports", {
-      method: "POST",
-      credentials: "same-origin",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({
-        submittedByUserId: 1000,
-        description: "debris",
-        eventType: report,
-        latitude: 50.1010,
-        longitude: 50.1010,
-        submittedAt: Date.now(),
-        status: "ACTIVE",
-        image: null,
+    const input = document.getElementById("report__description");
+    const imageUrl = document.getElementById("report__url");
+
+    try {
+      const location = await getLocation();
+
+      fetch("/api/reports", {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          submittedByUserId: 1000,
+          description: input.value,
+          eventType: report,
+          latitude: location.latitude,
+          longitude: location.longitude,
+          submittedAt: Date.now(),
+          status: "ACTIVE",
+          image: imageUrl.value,
+        })
       })
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => console.log(error)) ;
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+    } catch (error) {
+      console.log(error);
+      alert("Couldn't send report");
+    }
   })
 })
 
-// return ResponseEntity.status(HttpStatus.CREATED).body(newReport);
