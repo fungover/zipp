ba82adf5a8457921a98d548a909d5966f55e8527
Louise <emmalouisekarlsson@hotmail.com>
2025-12-11 11:28:30 +0100

    Function to get reporters position with either GPS or IP

--- backend/src/main/resources/static/js/getLocation.js
+++ backend/src/main/resources/static/js/getLocation.js
@@ -1,36 +1,34 @@
-export default async function getLocation(){
+export default function getLocation(){
 
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 15000,
-    maximumAge: 25000,
-  }
+  return new Promise(async(resolve, reject) => {
 
-  navigator.geolocation.getCurrentPosition(success, error, options)
-
-  function success(position){
-    const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
-    console.log(latitude, longitude)
-    return{latitude: latitude, longitude: longitude};
-  }
-
-  async function error(position) {
-    alert("GPS isn't available, attempt with IP address will be made");
-    return await getIpLocation();
-  }
-
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        resolve({latitude: position.coords.latitude, longitude: position.coords.longitude});
+      },
+      async ()=>{
+        alert("GPS isn't available, attempt with IP address will be made to get estimated position");
+        try{
+          resolve (await getIpLocation());
+        }catch(error){
+          reject(new Error("Location with IP address failed: " + error.message));
+        }
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 25000,
+      }
+    )
+  });
 }
 
 async function getIpLocation() {
-await fetch('http://ip-api.com/json/?fields=61439')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      const latitude = data.latitude;
-      const longitude = data.longitude;
-      return {latitude: latitude, longitude: longitude};
-    })
-    .catch(error => console.log(error))
+ const response = await fetch('http://ip-api.com/json/?fields=61439');
+ if(!response.ok){
+   throw Error(response.statusText);
+ }
+ const data = await response.json();
+ const {lat, lon} = data;
+  return {latitude: lat, longitude: lon};
 }
-
