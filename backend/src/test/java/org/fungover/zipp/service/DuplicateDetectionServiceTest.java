b16378280bc4e9debc26e87dfbbb5819118cc8b6
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-12-15 13:39:53 +0100

    Quick fix

--- backend/src/test/java/org/fungover/zipp/service/DuplicateDetectionServiceTest.java
+++ backend/src/test/java/org/fungover/zipp/service/DuplicateDetectionServiceTest.java
@@ -20,7 +20,10 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 public class DuplicateDetectionServiceTest {
 
