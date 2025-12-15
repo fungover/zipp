2be4cee636924e8f66b71c8017bbfd90d8da0e92
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-12-15 12:48:42 +0100

    Added method to query reports by event type, status and when it was submitted. Also added the DuplicateDetectionService

--- backend/src/main/java/org/fungover/zipp/repository/ReportRepository.java
+++ backend/src/main/java/org/fungover/zipp/repository/ReportRepository.java
@@ -1,10 +1,13 @@
 package org.fungover.zipp.repository;
 
+import org.fungover.zipp.dto.Report;
 import org.fungover.zipp.entity.ReportStatus;
 import org.fungover.zipp.entity.ReportEntity;
+import org.fungover.zipp.entity.ReportType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
@@ -14,4 +17,6 @@ public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
     List<ReportEntity> findAllBySubmittedByEmail(String email);
 
     List<ReportEntity> findAllByStatusAndSubmittedAtBefore(ReportStatus reportStatus, Instant expirationThreshold);
+
+    List<ReportEntity> findAllByEventTypeAndStatusAndSubmittedAtAfter(ReportType eventType, ReportStatus status, Instant submittedAfter);
 }
