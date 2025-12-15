be99b07f247bceb8ed38e9a132af9d89d13f4539
Kai <ericson.kai.o@gmail.com>
2025-12-15 14:53:02 +0100

    Make change suggested by sonar cloud

--- backend/src/test/java/org/fungover/zipp/kafka/KafkaEventsTest.java
+++ backend/src/test/java/org/fungover/zipp/kafka/KafkaEventsTest.java
@@ -26,7 +26,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class KafkaEventsTest {
+class KafkaEventsTest {
 
     @InjectMocks
     private ReportController reportController;
