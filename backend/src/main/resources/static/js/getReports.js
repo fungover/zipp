d4c187b6306725db882a92e0e4aa4410719685fe
Louise <emmalouisekarlsson@hotmail.com>
2025-12-17 19:18:51 +0100

    Solved issue with overwriting

--- backend/src/main/resources/static/js/getReports.js
+++ backend/src/main/resources/static/js/getReports.js
@@ -13,16 +13,36 @@ mapButton.addEventListener("click", async function (event) {
 
     const data = await response.json()
 
+    const mapElement = document.getElementById("map");
+
+    const map = new google.maps.Map(mapElement, {
+      zoom: 12
+    });
+
+    const bounds = new google.maps.LatLngBounds();
+
     data.forEach((res) => {
       const {latitude, longitude, eventType} = res;
       const event = eventType;
       const lat = parseFloat(latitude);
       const lng = parseFloat(longitude);
-      loadMap(lat, lng, event);
+
+      const position = {lat: lat, lng: lng};
+
+      new google.maps.Marker({
+        position: position,
+        map: map,
+        title: event
+      });
+
+      bounds.extend(position);
     })
 
-    const map = document.getElementById("map");
-    map.scrollIntoView({
+    if (data.length > 0) {
+      map.fitBounds(bounds);
+    }
+
+    mapElement.scrollIntoView({
       behavior: "smooth",
       block: "center",
     })
@@ -32,18 +52,4 @@ mapButton.addEventListener("click", async function (event) {
     alert("Couldn't retrieve any reports");
   }
 })
-
-function loadMap(lat, lng, event) {
-
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat, lng},
-    zoom: 12
-  });
-
-  new google.maps.Marker({
-    position: {lat, lng},
-    map: map,
-    title: event
-  });
-}
 }
