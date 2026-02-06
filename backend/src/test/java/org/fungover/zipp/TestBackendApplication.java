2a2655a35edc7a0ee5de45b50548e6474ae85cea
Patseroni <patrik.bustad@lerniastudent.se>
2026-02-06 02:49:20 +0100

    Upgrade HealthController test to a basic integration test with H2 persistence.

--- backend/src/test/java/org/fungover/zipp/TestBackendApplication.java
+++ backend/src/test/java/org/fungover/zipp/TestBackendApplication.java
@@ -1,10 +1,10 @@
-//package org.fungover.zipp;
-//
-//import org.springframework.boot.SpringApplication;
-//
-//public class TestBackendApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.from(BackendApplication::main).with(TestcontainersConfiguration.class).run(args);
-//    }
-//}
+package org.fungover.zipp;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(BackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+}
